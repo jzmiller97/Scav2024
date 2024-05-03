2f8d341be45391093c64ce1add44bfcda4e1ee73
Joshua Miller <81668597+intheaggregate@users.noreply.github.com>
2024-05-03 18:50:39 -0500

    Finished game framework.

--- dnd_duolingo.py
+++ dnd_duolingo.py
@@ -2,19 +2,25 @@ import random
 from quiz_games import Quiz
 import openpyxl
 import sys
+import re
+
+def true_length(text):
+    escape_regex = re.compile(r'\x1b\[[0-9;]*m')
+    clean_text = escape_regex.sub('', text)
+    return len(clean_text)
 
 def string_hearts(num_hearts) -> str:
-    hearts: str = '\u001b[31m\u2764\u001b[0m '  # ANSI escape codes for red heart
+    hearts: str = '\u001b[31m❤\u001b[0m '  # ANSI escape codes for red heart
     return hearts * num_hearts
 
-def string_box(text):
-    width: int = len(text) + 4
-    l1: str = "\u001b[1m\u250f" + "\u2501" * width + "\u2513\u001b[0m"
-    l2: str = "\u001b[1m\u2503  " + text + "  \u2503\u001b[0m"
-    l3: str = "\u001b[1m\u2517" + "\u2501" * width + "\u251b\u001b[0m"
+def string_box(text, width: int):
+    gap = abs(width - true_length(text))
+    l1 = "\u001b[1m┏" + "━" * (width + 1) + "┓\u001b[0m"
+    l2 = "\u001b[1m┃ " + text + " " * gap + "┃\u001b[0m"
+    l3 = "\u001b[1m┗" + "━" * (width + 1) + "┛\u001b[0m"
     return "\n".join((l1, l2, l3))
 
-def read_spreadsheet_to_dict(filename):
+def read_spreadsheet_to_dict(filename: str) -> dict[str, str]:
     data_dict: dict[str, str] = {}
     try:
         workbook = openpyxl.load_workbook(filename)
@@ -29,18 +35,15 @@ def read_spreadsheet_to_dict(filename):
         print("An error occurred:", e)
     return data_dict
 
-def read_spreadsheet_to_set(filename):
-    data_set: set[str] = {}
-    try:
-        workbook = openpyxl.load_workbook(filename)
-        sheet = workbook.active
-        for row in sheet.iter_rows(values_only=True):
-            value = str(row[0])
-            data_set.add(value)
-    except FileNotFoundError:
-        print(f"File '{filename}' not found.")
-    except Exception as e:
-        print("An error occurred:", e)
+def read_spreadsheet_to_set(filename: str) -> set[str]:
+    data_set: set[str] = set()
+    workbook = openpyxl.load_workbook(filename)
+    for sheet in workbook.sheetnames:
+        current_sheet = workbook[sheet]
+        for row in current_sheet.iter_rows():
+            for cell in row:
+                if cell.value is not None:
+                    data_set.add(str(cell.value))
     return data_set
 
 def print_sword_greeting():
@@ -54,12 +57,7 @@ def print_sword_greeting():
     print("\u001b[33m")  # ANSI escape code for yellow color
     for line in sword:
         print(line)
-    print("\u001b[0m")  # Reset color
-
-questions: dict[str, str] = read_spreadsheet_to_dict
-praises: set[str] = read_spreadsheet_to_set
-insults: set[str] = read_spreadsheet_to_set
-motivations: set[str] = read_spreadsheet_to_set
+    print("\u001b[0m")  # Reset color6
 
 class DnD(Quiz):
     hp: int
@@ -75,46 +73,53 @@ class DnD(Quiz):
         self.questions = questions
         self.praises = praises
         self.insults = insults
-        self.motivations: motivations
+        self.motivations = motivations
         self.hp = hp
         self.hp_max = hp
         self.streak = 0
     
     def ask_question(self) -> None:
         question = random.choice(list(self.questions.keys()))
-        motivation = random.choice(self.motivations)
+        motivation = random.choice(list(self.motivations))
         answer = input(question + " " + motivation + " ")
         self.check_answer(question, answer)
 
     def check_answer(self, question, user_answer) -> None:
         correct_answer = self.questions[question]
         if user_answer.lower() == correct_answer.lower():
-            streak += 1
-            if streak % 5 == 0:
-                hp += 1
-            print(random.choice(self.praises))
+            self.streak += 1
+            if self.streak % 5 == 0:
+                self.hp += 1
+            print(random.choice(list(self.praises)))
         else:
-            streak = 0
-            hp += -1
-            print(random.choice(self.insults) + " The correct answer is:", correct_answer)
+            self.streak = 0
+            self.hp += -1
+            print(random.choice(list(self.insults)) + " The correct answer is:", correct_answer)
             
     def reset(self) -> None:
         self.streak = 0
         self.hp = self.hp_max
         
-    def print_gamestate(self) -> None:
-        print(string_box("Hit points remaining: " + string_hearts(self.hp)))
-        print("Current streak: " + self.streak + ".")
+    def print_gamestate(self, width: int) -> None:
+        inp: str = "Hit points remaining: " + string_hearts(self.hp)
+        print(string_box(inp, width))
+        print("Current streak: " + str(self.streak) + ".")
         
 
 if __name__ == "__main__":
     print_sword_greeting()
-    quiz = DnD()
+    dnd_questions: dict[str, str] = read_spreadsheet_to_dict("dnd_questions.xlsx")
+    dnd_motivations: set[str] = read_spreadsheet_to_set("dnd_motivations.xlsx")
+    dnd_praises: set[str] = read_spreadsheet_to_set("dnd_praises.xlsx")
+    dnd_insults: set[str] = read_spreadsheet_to_set("dnd_insults.xlsx")
+    quiz: "DnD" = DnD(dnd_questions, dnd_praises, dnd_insults, dnd_motivations, int(sys.argv[1]))
+    width = true_length("Hit points remaining: " + string_hearts(quiz.hp)) + 2
     while True:
+        quiz.print_gamestate(width)
         quiz.ask_question()
         if quiz.hp <= 0:
             play_again: str = input("Re-roll initiative? (yes/no): ")
-        if play_again.lower() != "yes":
-            quiz.reset()
-        else:
-            break
+            if play_again.lower() == "yes":
+                quiz.reset()
+            else:
+                break
